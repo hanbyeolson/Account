b95e8fd5bd431a6cab511f0d3b9baed1a423bd47
hanbyeolson <gksquf7238@naver.com>
2022-12-07 23:18:22 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/fastlms2/Fastlms2Application.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fastlms2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fastlms2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/fastlms2/Fastlms2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.fastlms2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fastlms2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
